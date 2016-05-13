0f031b19f31a1b51ef76ef07f61436b542a4997d
Fioger <fioger95@Live.com>
2016-05-13 16:54:39 -0400

    X-axis is now the years

--- ex1.js
+++ ex1.js
@@ -1,19 +1,24 @@
+var x_ax = ['x']
+for (var i = 1974; i<= 2011; i++){
+    x_ax.push(i.toString());
+};
+console.log(x_ax);
+
 var chart = c3.generate({
     data: {
-        x: 'x',
-//        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+        url: 'test.csv',
+        x: 'year',
         columns: [
-            ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
-//            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 130, 340, 200, 500, 250, 350]
+            x_ax
         ]
+
     },
     axis: {
         x: {
-            type: 'timeseries',
             tick: {
-                format: '%Y-%m-%d'
+
+                // this also works for non timeseries data
+                // values: ['2013-01-05', '2013-01-10']
             }
         }
     }
