2ae63064b85a4ef9dd938c1068ec9c0544999081
Porrith Suong <psuong95@gmail.com>
2016-05-14 12:41:04 -0400

    Restructures the JSON Data.

--- js/astoria_json.js
+++ js/astoria_json.js
@@ -2,5 +2,36 @@ $(document).ready(function () {
     $.getJSON("../data/grouped_neighborhoods.json", function (data) {
         console.log(data);
         console.log(data['Astoria']);
+
+        var neighborhood_data = c3.generate({
+            bindto: '#median-housing',
+            data: {
+                json: data['Baychester'],
+                keys: {
+                    x: 'year',
+                    y: 'price',
+                    value: ['familysize', 'price', 'year']
+                }
+            },
+            axis: {
+                x: {
+                    label: {
+                        text: 'Year'
+                    },
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y'
+                    },
+                    show: true
+                },
+                y: {
+                    label: {
+                        text: 'Price'
+                    },
+                    show: true
+                }
+            }
+        });
     });
+
 });
