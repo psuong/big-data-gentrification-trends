48f14eb9866c45fd5f3ce564297798f67912cd4f
Porrith Suong <psuong95@gmail.com>
2016-05-15 13:21:54 -0400

    Creates function to append to neighborhoods list.

--- app.py
+++ app.py
@@ -6,4 +6,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    context_dict = {
+        'neighborhoods': create_neighborhood_list()
+    }
     pass
+
+
+def create_neighborhood_list():
+    """
+    Reads the textfile for all neighborhoods currently logged and appends it
+    to a list.
+    :return: list
+    """
+    neighborhood_list = []
+    with open('neighborhoods.txt', 'r') as neighborhoods:
+        for neighborhood in neighborhoods:
+            neighborhood_list.append(neighborhood)
+    return neighborhood_list
